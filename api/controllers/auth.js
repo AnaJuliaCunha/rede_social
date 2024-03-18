35456da4fd59f1141aaac439fe2f92eaec24939d
AnaJuliaCunha <anajuliacunha15@gmail.com>
2024-03-18 17:10:19 -0300

    fix

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,38 +1,64 @@
-import { db } from "../connect"
-import bcrypt from "bcrypt"
+import { db } from "../connect.js";
+import bcrypt from "bcrypt";
 
 export const register = (req, res) => {
-    const {username, email, password, confirmPassword} = req.body
+  const { username, email, password, confirmPassword } = req.body;
 
-    if (!username){
-        return res.status(422).json({msg: "O nome é obrigatório!"})
-    }
+  if (!username) {
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  }
 
-    if (!email){
-        return res.status(422).json({msg: "O email é obrigatório!"})
-    }
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
 
-    if (!password){
-        return res.status(422).json({msg: "A senha é obrigatório!"})
-    }
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatório!" });
+  }
 
-    if (password !=  confirmPassword){
-        return res.status(422).json({msg: "As senhas não são iguais!"})
-    }
+  if (password != confirmPassword) {
+    return res.status(422).json({ msg: "As senhas não são iguais!" });
+  }
 
-    db.query("SELECT email FROM user WHERE email = ?"[email], async(error, data)=>{
-        if(error){
-            console.log(error)
-            return res.status(500).json({msg: "Aconteceu algum erro no servidor. Tente novamente mais tarde."})
-            if(data.length > 0){
-                return res.status(500).json({msg: "Este email já está sendo utilizado"})
-            }else{
-                const passwordHash = await b
+  db.query(
+    "SELECT email FROM user WHERE email = ?",
+    [email],
+    async (error, data) => {
+      if (error) {
+        console.log(error);
+        return res
+          .status(500)
+          .json({
+            msg: "Aconteceu algum erro no servidor. Tente novamente mais tarde.",
+          });
+        if (data.length > 0) {
+          return res
+            .status(500)
+            .json({ msg: "Este email já está sendo utilizado" });
+        } else {
+          const passwordHash = await bcrypt.hash(password, 8);
+          db.query(
+            "INSERT INTO user SET ?",
+            { username, email, password: passwordHash },
+            (error) => {
+              if (error) {
+                console.log(error);
+                return res
+                  .status(500)
+                  .json({
+                    msg: "Aconteceu algum erro no servidor. Tente novamente mais tarde.",
+                  });
+              } else {
+                return res
+                  .status(200)
+                  .json({ msg: "Usuário cadastrado com sucesso!" });
+              }
             }
+          );
         }
-    })
-}
+      }
+    }
+  );
+};
 
-export const login = (req, res) => {
-    
-}
+export const login = (req, res) => {};
